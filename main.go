87b7d8be3ffa2e345c3e097c462b2dbcd033c930
caledfwlch1 <caledfwlch1@gmail.com>
2016-05-16 16:44:53 +0300

    parse html tags

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"golang.org/x/net/html"
 	"io/ioutil"
 	"bytes"
+	"html/template"
 )
 
 const (
 	pagename = "http://www.npp/"
 )
+
+type Page struct {
+	Link	string
+}
 
 func main() {
 	fmt.Println("Start.")
@@ -22,12 +27,17 @@ func main() {
 
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get(pagename)
+	Linkpage := pagename
+	p := &Page{Link: Linkpage}
+	renderTemplate(w, p)
+
+	resp, err := http.Get(Linkpage)
 	if err != nil {
 		fmt.Fprint(w, err)
 	}
 	rob, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
+
 	doc, err := html.Parse(bytes.NewReader(rob))
 	par(doc, w)
 
@@ -47,4 +57,19 @@ func par (n *html.Node, w http.ResponseWriter) {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		par(c, w)
 	}
+}
+
+
+
+
+func renderTemplate(w http.ResponseWriter, p *Page) {
+	var err error
+	TemplateRep := template.Must(template.ParseFiles("index.html")) // шаблон для первой страницы и страницы с результатом поиска
+
+	err = TemplateRep.ExecuteTemplate(w, "index.html", p)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	return
 }
